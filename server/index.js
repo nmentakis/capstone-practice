28db1f942fde9eccc1c6e39a324270717e0c78e5
nmentakis <nmentakis@gmail.com>
2023-11-14 11:29:16 -0600

    Add feature to provide dog information

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get("/dogs", () => {
+    res.send("Maybe one or two or all the dogs?");
+})
+
 app.listen(3001, () => {
     console.log("connected on port 3001");
 });
